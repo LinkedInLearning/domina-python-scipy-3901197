4c331714016f78ed617a46a896ca6138e6c5f26e
lincgr <lincgr@gmail.com>
2024-10-09 10:41:15 +0200

    01_01 Crear una matriz CSR en SciPy

---
+++ 01_01/01_01.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+calificaciones_peliculas = np.array([
+    [5, 0, 0, 0, 3],
+    [0, 4, 0, 2, 0],
+    [0, 0, 0, 0, 1],
+    [1, 0, 3, 0, 0],
+    [0, 2, 0, 0, 0]
+])
