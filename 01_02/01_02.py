6445f2e0a3e287c9d6e044d94928085978c9bdc2
lincgr <lincgr@gmail.com>
2024-10-09 10:49:40 +0200

    01_02 Suma y multiplicación de  matrices CSR con SciPy

---
+++ 01_02/01_02.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+datos_usuarios = [1, 2, 3, 4]
+indices_columna_usuarios = [0, 2, 2, 0]
+indptr_usuarios = [0, 2, 3, 4]
+
+matriz_usuarios = csr_matrix((datos_usuarios, indices_columna_usuarios, indptr_usuarios), shape=(3, 3))
+
+datos_productos = [5, 6, 7]
+indices_columna_productos = [1, 0, 2]
+indptr_productos = [0, 1, 2, 3]
+
+matriz_productos = csr_matrix((datos_productos, indices_columna_productos, indptr_productos), shape=(3, 3))
