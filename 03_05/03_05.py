2c8cbff25a8c2cd387aef02170a2c4a513edfc44
lincgr <lincgr@gmail.com>
2024-10-10 14:18:37 +0200

    03_05 Datos espaciales de SciPy

---
+++ 03_05/03_05.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+
+escuelas = np.array([
+    [37.77, -122.42], 
+    [37.78, -122.43], 
+    [37.76, -122.45], 
+    [37.79, -122.40] 
+])
+
+hospitales = np.array([
+    [37.80, -122.41], 
+    [37.74, -122.44],
+])
